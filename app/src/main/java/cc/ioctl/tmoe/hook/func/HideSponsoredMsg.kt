adb8e016c1496f0babb6fccf05ab20453147d2e3
rhjdvsgsgks <26178113+rhjdvsgsgks@users.noreply.github.com>
2026-07-18 07:55:33 +0000

    Update HideSponsoredMsg hook from Killergram
    https://github.com/shatyuka/Killergram/commit/d8faedca5557fd12b86b79510cfbfc1be8d2034a

--- app/src/main/java/cc/ioctl/tmoe/hook/func/HideSponsoredMsg.kt
+++ app/src/main/java/cc/ioctl/tmoe/hook/func/HideSponsoredMsg.kt
@@ -3,18 +3,23 @@ package cc.ioctl.tmoe.hook.func
 import cc.ioctl.tmoe.hook.base.CommonDynamicHook
 import com.github.kyuubiran.ezxhelper.utils.hookBefore
 import com.github.kyuubiran.ezxhelper.utils.loadAndFindMethods
-import com.github.kyuubiran.ezxhelper.utils.tryOrFalse
+import com.github.kyuubiran.ezxhelper.utils.tryOrLogFalse
+import com.github.kyuubiran.ezxhelper.utils.findMethod
+import com.github.kyuubiran.ezxhelper.utils.loadClass
 
 /**
  * From `https://github.com/shatyuka/Killergram`
  */
 object HideSponsoredMsg : CommonDynamicHook() {
-    override fun initOnce(): Boolean = tryOrFalse {
+    override fun initOnce(): Boolean = tryOrLogFalse {
         arrayOf(
             "org.telegram.messenger.MessagesController",
             "org.telegram.ui.ChatActivity"
         ).loadAndFindMethods {
             name.contains("SponsoredMessages")
+        }.hookBefore { if (isEnabled) it.result = null }
+        findMethod(loadClass("org.telegram.tgnet.TLRPC\$messages_SponsoredMessages")) {
+            name == "TLdeserialize"
         }.hookBefore { if (isEnabled) it.result = null }
     }
 }
