c9632a6d959e4ce5ce827c422b8e62ab0a854cd8
rhjdvsgsgks <26178113+rhjdvsgsgks@users.noreply.github.com>
2026-07-18 15:46:55 +0000

    add FunctionHookEntry for HideSponsoredMsg

--- app/src/main/java/cc/ioctl/tmoe/hook/func/HideSponsoredMsg.kt
+++ app/src/main/java/cc/ioctl/tmoe/hook/func/HideSponsoredMsg.kt
@@ -1,6 +1,7 @@
 package cc.ioctl.tmoe.hook.func
 
 import cc.ioctl.tmoe.hook.base.CommonDynamicHook
+import cc.ioctl.tmoe.base.annotation.FunctionHookEntry
 import com.github.kyuubiran.ezxhelper.utils.hookBefore
 import com.github.kyuubiran.ezxhelper.utils.loadAndFindMethods
 import com.github.kyuubiran.ezxhelper.utils.tryOrLogFalse
@@ -10,6 +11,7 @@ import com.github.kyuubiran.ezxhelper.utils.loadClass
 /**
  * From `https://github.com/shatyuka/Killergram`
  */
+@FunctionHookEntry
 object HideSponsoredMsg : CommonDynamicHook() {
     override fun initOnce(): Boolean = tryOrLogFalse {
         arrayOf(
